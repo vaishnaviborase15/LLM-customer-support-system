497459e1f562e87992d5ddddadd2696c8fff3323
vaishnaviborase15 <vaishnaviborse.clg@gmail.com>
2026-03-27 01:16:27 +0530

    Deployment ready

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,9 +1,21 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # 🔥 Paste your Render DB URL here for testing
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+env_path = BASE_DIR / ".env"
+
+load_dotenv(dotenv_path=env_path)
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("🔥 DATABASE_URL:", DATABASE_URL)
+
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL not found. Check your .env file!")
 
 engine = create_engine(DATABASE_URL)
 
