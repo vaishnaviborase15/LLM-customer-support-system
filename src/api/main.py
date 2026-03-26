473b06bceff972781634464e1fe85be7564352d1
vaishnaviborase15 <vaishnaviborse.clg@gmail.com>
2026-03-27 01:22:17 +0530

    backend ready for deployment

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from src.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Customer Support AI API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routes
 app.include_router(router)
